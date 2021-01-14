2cd37cdb99655a9ffa67b11836a365982f38ba89
Alexey Savchenko <alexey.savchenko@gen.tech>
2021-01-14 15:25:50 +0200

    Initial Commit

---
+++ StoryCropper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryCropper
+//
+//  Created by Alexey Savchenko on 14.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
